efaa3fdf4428318fa3635727e88a7a0a4c241f14
praiseatieno-rgb <praiseatie71@gmail.com>
2026-06-17 11:45:35 +0300

    added update

---
+++ lesson1b.py
@@ -0,0 +1,20 @@
+# python data types
+# when we talk of data type, we mean the contect held inside a variable
+# Integers - this indicates a whole number is python. Note, when indicate an integer, we dont use quatation marks
+
+age = 36
+print (age)
+print(type(age))
+
+
+# a float is a numer that has a decimal
+pi = 3.142
+print(pi)
+print(type(pi))
+
+#string - this is a sereies/sequence of characters that are enclosed in 
+
+name = "Mary"
+
+print(name)
+print(type(name))
--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 print("hello, python instalation successfull ...")
 
+message = "hello there, hope you are doing good?"
 
+print(message)
 
+message2 = "i love programming"
+print(message2)
 
-
-
-# for = john 
+message3 =message + message2
+print(message3)
