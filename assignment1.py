49d4747b0d06a4d264c7e35f6bae2c7ecff8d2cd
praiseatieno-rgb <praiseatie71@gmail.com>
2026-06-17 15:21:58 +0300

    assignment one update

---
+++ assignment1.py
@@ -0,0 +1,27 @@
+#Calculate the simple intrest
+
+p = 50000
+r = 5
+t = 8
+
+SI = (p * r * t) / 100
+print("Simple Interest:", SI)
+
+#Calculate the BMI of a person
+
+weight = 78
+height = 1.75
+
+BMI = weight / (height ** 2)
+print("BMI is:",BMI)
+
+#The Area and Perimeter of a Rectangle
+
+length = 48
+width = 25
+
+area = length * width
+print("the Area is:", area)
+
+perimeter = 2 * (length + width)
+print("The Perimeter is:", perimeter)
