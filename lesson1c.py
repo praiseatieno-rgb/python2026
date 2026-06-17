0695bfd33a27247169f1dffd03114c8d43f89f89
praiseatieno-rgb <praiseatie71@gmail.com>
2026-06-17 14:49:56 +0300

    updates on lessons

---
+++ lesson1c.py
@@ -0,0 +1,44 @@
+#Arithmetic Operations
+# Addition (+)
+
+a = 45
+b = 60 
+answer = a + b
+print("the sum of the number is", answer)
+
+# Subtraction 
+
+x = 85
+y = 40 
+difference = x - y
+print("the difference of number is", difference)
+
+#division (/)
+
+e = 15 
+f = 5 
+division = e / f 
+print ("the division of the number is", division)
+
+# Multipication (*)
+
+m = 6
+t = 5 
+product = m * t
+print("the product of the numbers is:", product)
+
+# Modulus (%) - this is the remainder that you get when a number subdivides another number
+numberx = 47
+numbery =7
+modulus = numberx % numbery
+print("the module of the number is:", modulus)
+
+#four division (//) - this is the number of times a number subdivides another ignoring the remender
+
+floor = numberx // numbery
+print("the anser is:", floor)
+
+# Exponent (**) - this is the power factor
+numbern = 10
+exponent = numbern ** 2
+print("the exponent of the number is:", exponent)
